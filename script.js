f2326d433e4314ebc1f0b4e5424880c32d8351e4
JubayerAhmedSamrat <jasamrat@gmail.com>
2024-11-02 13:34:59 +0600

    fixed

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
 const randomUser = () =>{
     fetch('https://randomuser.me/api/?results=50')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => showUser(data.results))
 }
 
-const showUser = (values)=>{
-    const mainDiv = document.getElementById('main-div')
-    const result = values.forEach(value =>{
-        console.log(value)
-        const div = document.createElement('div')
-        div.style.border = '2px solid black'
-        div.innerHTML =`<p>${value.name.title} ${value.name.first}${value.name.last}</p>
-        <img src="${value.picture.thumbnail}" alt="img" />
+const showUser = (values) =>{
+    const mainDiv = document.getElementById('main-div');
+    const result = values.forEach( value =>{
+        console.log(value);
+        const div = document.createElement('div');
+        div.style.border = '2px solid black';
+        div.innerHTML = `
+        <p>${value.name.title} ${value.name.first}${value.name.last}</p>
+        <img src="${value.picture.thumbnail}" alt="img"/>
         <button onclick="anotherFunction(${value.login.uuid})">Click me</button>
-        `
-        mainDiv.appendChid(div)
+        `;
+        mainDiv.appendChild(div);
     })
 }
-
-const anotherFunction = (value)=>{
-    console.log(value)
+const anotherFunction = (value) =>{
+    console.log(value);
 }
 randomUser();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const showUser = (values)=>{
+//     const mainDiv = document.getElementById('main-div')
+//     const result = values.forEach(value =>{
+//         console.log(value)
+//         const div = document.createElement('div')
+//         div.style.border = '2px solid black'
+//         div.innerHTML =`<p>${value.name.title} ${value.name.first}${value.name.last}</p>
+//         <img src="${value.picture.thumbnail}" alt="img" />
+//         <button onclick="anotherFunction(${value.login.uuid})">Click me</button>
+//         `
+//         mainDiv.appendChid(div)
+//     })
+// }
+
+// const anotherFunction = (value)=>{
+//     console.log(value)
+// }
